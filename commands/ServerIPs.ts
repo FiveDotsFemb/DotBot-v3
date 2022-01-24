65ed4b6b7cfcfee8765fa861e27af125bce3146b
Dots <96595352+FiveDotsFemb@users.noreply.github.com>
2022-01-24 20:23:30 +0200

    formatting

--- commands/ServerIPs.ts
+++ commands/ServerIPs.ts
@@ -2,66 +2,67 @@ import { MessageEmbed, MessageSelectMenu } from "discord.js";
 import { ICommand } from "wokcommands";
 
 export default {
-    category: 'General',
-    description: 'Sends an Embed of the Femboy KZ servers and their IPs',
-    slash: 'both',
-    testOnly: true,
-    permissions: ['ADMINISTRATOR'],
+  category: "General",
+  description: "Sends an Embed of the Femboy KZ servers and their IPs",
+  slash: "both",
+  testOnly: true,
+  permissions: ["ADMINISTRATOR"],
 
-    callback: async ({ message, text }) => {
-        const embed = new MessageEmbed()
-            .setTitle('All current Femboy KZ Servers and their IPs')
-            .setDescription('** **')
-            .setColor('#FF33AC')
-            .addFields([{
-                name: 'Whitelist Servers',
-                value: '** **'
-            },
-            {
-                name: ':flag_eu:  **[Global] - 128tick**',
-                value: '135.181.58.159:27040',
-                inline: true,
-            },
-            {
-                name: ':flag_eu:  **[Global] - 128tick**',
-                value: '135.181.58.159:27045',
-                inline: true,
-            },
-            {
-                name: ':flag_eu:  **64tick**',
-                value: '135.181.58.159:27041',
-            },
-            {
-                name: 'VIP Servers',
-                value: '** **',
-            },
-            {
-                name: ':flag_eu:  **Autobhop - 128tick**',
-                value: '135.181.58.159:27043',
-                inline: true,
-            },
-            {
-                name: ':flag_eu:  **Autobhop - 102.4tick**',
-                value: '135.181.58.159:27044',
-                inline: true,
-            },
-            {
-                name: 'Public Servers',
-                value: '** **',
-            },
-            {
-                name: ':flag_eu:  **[Global] - 128tick**',
-                value: '135.181.58.159:27046',
-                inline: true,
-            },
-            {
-                name: ':flag_eu:  **Autobhop - 64tick**',
-                value: '135.181.58.159:27042',
-                inline: true,
-            },
-        ])
-        const newMessage = await message.channel.send({
-            embeds: [embed]
-        })
-    }
-} as ICommand
+  callback: async ({ message, text }) => {
+    const embed = new MessageEmbed()
+      .setTitle("All current Femboy KZ Servers and their IPs")
+      .setDescription("** **")
+      .setColor("#FF33AC")
+      .addFields([
+        {
+          name: "Whitelist Servers",
+          value: "** **",
+        },
+        {
+          name: ":flag_eu:  **[Global] - 128tick**",
+          value: "135.181.58.159:27040",
+          inline: true,
+        },
+        {
+          name: ":flag_eu:  **[Global] - 128tick**",
+          value: "135.181.58.159:27045",
+          inline: true,
+        },
+        {
+          name: ":flag_eu:  **64tick**",
+          value: "135.181.58.159:27041",
+        },
+        {
+          name: "VIP Servers",
+          value: "** **",
+        },
+        {
+          name: ":flag_eu:  **Autobhop - 128tick**",
+          value: "135.181.58.159:27043",
+          inline: true,
+        },
+        {
+          name: ":flag_eu:  **Autobhop - 102.4tick**",
+          value: "135.181.58.159:27044",
+          inline: true,
+        },
+        {
+          name: "Public Servers",
+          value: "** **",
+        },
+        {
+          name: ":flag_eu:  **[Global] - 128tick**",
+          value: "135.181.58.159:27046",
+          inline: true,
+        },
+        {
+          name: ":flag_eu:  **Autobhop - 64tick**",
+          value: "135.181.58.159:27042",
+          inline: true,
+        },
+      ]);
+    const newMessage = await message.channel.send({
+      embeds: [embed],
+    });
+  },
+} as ICommand;
--- index.ts
+++ index.ts
@@ -1,36 +1,32 @@
-import DiscordJS, { ContextMenuInteraction, Intents } from 'discord.js'
-import WOKCommands from 'wokcommands'
-import path from 'path'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import testSchema from './test-schema'
-dotenv.config()
-
+import DiscordJS, { ContextMenuInteraction, Intents } from "discord.js";
+import WOKCommands from "wokcommands";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import testSchema from "./test-schema";
+dotenv.config();
 
 const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_PRESENCES,
-    ]
-})
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ],
+});
 
-client.on('ready', async () => {
-    await mongoose.connect(
-        process.env.MONGO_URI || '',
-         {
-             keepAlive: true,
-         })
+client.on("ready", async () => {
+  await mongoose.connect(process.env.MONGO_URI || "", {
+    keepAlive: true,
+  });
 
-    new WOKCommands(client, {
-        commandDir: path.join(__dirname, 'commands'),
-        typeScript: true,
-        testServers: ['752232440579227848', '858419058135662622'],
-        botOwners: ['289767921956290580'],
-        // mongoUri: process.env.MONGO_URI,
-    })
-})
+  new WOKCommands(client, {
+    commandDir: path.join(__dirname, "commands"),
+    typeScript: true,
+    testServers: ["752232440579227848", "858419058135662622"],
+    botOwners: ["289767921956290580"],
+    // mongoUri: process.env.MONGO_URI,
+  });
+});
 
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
